224ed46fbb07bf5cf08054934c034084ab0e57ac
Lorraine <48839557+djmissmills@users.noreply.github.com>
2021-05-19 21:19:54 +0100

    add functions

--- functions/new.js
+++ functions/new.js
@@ -1,40 +1,51 @@
-required('dotenv').config();
-const faundadb = require('faunadb');
+require('dotenv').config();
+const faunadb = require('faunadb');
 const shortid = require('shortid');
 const axios = require('axios');
 const querystring = require('querystring');
 
 const q = faunadb.query;
 const client = new faunadb.Client({
-    secret: process.env.FAUNADB
+  secret: process.env.FAUNADB
 });
 
 module.exports.handler = async event => {
-    const data = querystring.parse(event.body);
-    const uniquePath = shortid.generate();
-    data.path = uniquePath;
+  const data = querystring.parse(event.body);
+  const uniquePath = shortid.generate();
+  data.path = uniquePath;
 
-    const page = {
-        data: data
-    };
+  const page = {
+    data: data
+  };
 
+  try {
+    const queryResponse = await client.query(
+      q.Create(
+        q.Collection('page'),
+        page
+      )
+    );
     try {
-        const queryRsponse = await client.query(
-            q.Create(
-                q.Collection('page'),
-                page
-            )
-        );
-        const response = {
-            statusCode: 200,
-            body: JSON.stringify(queryResponse)
-        }
-        return response;
-    } catch (error) {
-        const errorResponse = {
-            statusCode: 400,
-            body: JSON.stringify(error)
-        }
-        return errorResponse;
+      await axios.post('https://api.netlify.com/build_hooks/5e1c353a1a247acd66723af7')
+    } catch(error) {
+      console.error('Netlify Build Error', error);
     }
+
+    const response = {
+      statusCode: 302,
+      body: JSON.stringify(queryResponse),
+      headers: {
+        Location: `/mypage/${uniquePath}`
+      }
+    }
+    return response;
+  } catch(error) {
+    const errorResponse = {
+      statusCode: 400,
+      body: JSON.stringify(error)
+    }
+    return errorResponse;
+  }
+
+  
 }
